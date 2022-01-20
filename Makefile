0227cd7271cedde7500bbe0347932f79663b68a2
Jack Delahunt <20085409@mail.wit.ie>
2022-01-20 06:37:08 +0000

    Added glfw

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 build:
 	rm -rf build/
 	mkdir build
-	(cd build/ && cmake ../)
+	(cd build/ && cmake -DGLFW_BUILD_DOCS=OFF -DGLFW_INSTALL=OFF -S ../)
 	(cd build/ && make)
 
 .PHONY: run
--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "GLFW/glfw3.h"
 
 int main() {
-    printf("Hello world\n");
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(640, 480, "Wave Simulation", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while(true) {}
 }
